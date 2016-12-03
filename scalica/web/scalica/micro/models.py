1e5724acb520f9fba7d7d3da6f3e05757c8a84ec
algilardi <algilardi95@gmail.com>
2016-12-03 17:40:44 -0500

    updated models for hashtags

--- scalica/web/scalica/micro/models.py
+++ scalica/web/scalica/micro/models.py
@@ -24,6 +24,21 @@ class Following(models.Model):
   def __str__(self):
     return self.follower.username + "->" + self.followee.username
 
+class Hashtag(models.Model):
+  text = models.CharField(max_length=256)
+
+class PostTag(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+  posttag_date = models.DateTimeField('date')
+
+class Sentiment(models.Model):
+  hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+  sentiment_analysis = #what type should this be?
+  sentiment_date = models.DateTimeFild('sentiment date')
+  
+
+
 # Model Forms
 class PostForm(ModelForm):
   class Meta:
