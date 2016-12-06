e05bf43563474789d109ff5be29a34bf32c1e704
Braden Gammon <hello@bgammon.me>
2016-12-06 15:57:30 -0500

    Fix random-ass bracket in views thanks AL! :)

--- scalica/web/scalica/micro/views.py
+++ scalica/web/scalica/micro/views.py
@@ -1,4 +1,4 @@
-[from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
